cb2623cfe2e79b33c6e30c2f0f4e046f77851c7b
Gagan Malvi <malvigagan@gmail.com>
2020-12-09 19:45:16 +0530

    common: Add config.fs from LA.UM.9.15.r1-02000-KAMORTA.0
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,3 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+
+# Filesystem
+TARGET_FS_CONFIG_GEN += device/cygnus/common/configs/filesystem/config.fs
